7b43c284953b4be9b86feb46a4f8b73b76e332a0
AneeshAngadi <anishangadi@gmail.com>
2021-02-25 16:28:48 +0530

    Load mdbootstrap CSS and JS files

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Include CSS and JS files
+ */
+function theme_enqueue_scripts() {
+	wp_enqueue_style( 'Font_Awesome', 'https://use.fontawesome.com/releases/v5.6.1/css/all.css' );
+    wp_enqueue_style( 'MDB_CSS', get_template_directory_uri() . '/assets/css/mdb.min.css' );
+    wp_enqueue_script( 'MDB_JS', get_template_directory_uri() . '/assets/js/mdb.min.js', array(), '1.0.0', true );
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_scripts' );
+
+?>
