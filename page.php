618e5cf2389a257cd0fb03bf898bc109d2d9b22a
Aneesh Angadi <anishangadi@gmail.com>
2021-05-06 02:11:59 +0530

    Fix styling for page template

--- page.php
+++ page.php
@@ -1,12 +1,39 @@
 <?php
+/**
+ *
+ *Template for displaying pages
+ *
+ *
+ *
+ */
+
 get_header();
+?>
 
-  if ( have_posts() ) {
-    while ( have_posts() ) {
-    the_post();
-    the_content();
-    }
-  }
 
+<div class="container mt-3 px-lg-5">
+  <div class="row">
+    <div class="col-md-12">
+      <section class="px-2 px-lg-4">
+
+        <h1 class="font-weight-bold mb-4 text-center"><?php the_title() ?></h1>
+
+        <?php
+
+        if ( have_posts() ) {
+          while ( have_posts() ) {
+            the_post();
+            the_content();
+          }
+        }
+
+        ?>
+
+      </section>
+    </div>
+  </div>
+</div>
+
+<?php
 get_footer();
 ?>
